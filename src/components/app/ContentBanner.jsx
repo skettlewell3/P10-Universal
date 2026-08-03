8bd5672e56a964869a5c0e4af5e6787fbeb17d56
Sam K <113443285+skettlewell3@users.noreply.github.com>
2026-08-03 18:16:54 +0100

    centering

--- src/components/app/ContentBanner.jsx
+++ src/components/app/ContentBanner.jsx
@@ -12,7 +12,6 @@ export default function ContentBanner({ title }) {
             .toUpperCase()
     ;
 
-
     return (
         <div className="contentBanner">
             <button
@@ -26,7 +25,6 @@ export default function ContentBanner({ title }) {
             <div className="pageTitle">
                 {pageTitle}
             </div>
-
         </div>
     )
 }
